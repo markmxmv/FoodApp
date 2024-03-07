d91d97d34eaf5b5c5a45703d7afcc370d42a4111
Mark Maximov <markstopkidding@gmail.com>
2024-03-07 18:26:38 +0100

    Creating dish card and routing for it

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,4 +1,5 @@
 import Heading from '../../components/Heading/Heading';
+import ProductCard from '../../components/ProductCard/ProductCard';
 import Search from '../../components/Search/Search';
 import styles from './Menu.module.css';
 
@@ -10,6 +11,16 @@ export function Menu() {
 				<Heading>Menu</Heading>
 				<Search placeholder='Search dishes'/>
 
+			</div>
+			<div>
+				<ProductCard
+					id={1}
+					title='Enjoying'
+					description='Salami, ruccola, tomatoes, olives'
+					rating={4.5}
+					price={300}
+					image='/product-demo.png'
+				/>
 			</div>
 		</>
 	);
---
+++ src/pages/Product/Product.tsx
@@ -0,0 +1,12 @@
+import { useParams } from 'react-router-dom';
+
+export function Product() {
+
+	const {id} = useParams();
+
+	return (
+		<>
+            Product: {id}
+		</>
+	);
+}
